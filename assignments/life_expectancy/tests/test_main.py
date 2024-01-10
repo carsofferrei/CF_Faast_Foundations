af6ec06498cfaf1e42b77a4e73fd454e40c8f590
Carla Ferreira <140058356+carsofferrei@users.noreply.github.com>
2024-01-10 09:59:29 +0000

    changes in test_main

--- assignments/life_expectancy/tests/test_main.py
+++ assignments/life_expectancy/tests/test_main.py
@@ -12,8 +12,6 @@ def test_main(pt_life_expectancy_expected):
     """Run the `main` function and compare the output to the expected output
     Args:
         pt_life_expectancy_expected (Fixture) : load the expected output of the cleaning script
-        monkeypatch (MonkeyPatch): pytest fixture that provides a way to modify the behavior of 
-            functions or classes during tests.
     Returns:
     """
     # Call the main function and get the result
