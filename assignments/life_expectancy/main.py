b869da5c38c50ba8b74872023c4071fb80581b67
Carla Ferreira <140058356+carsofferrei@users.noreply.github.com>
2024-02-08 23:24:48 +0000

    Define design patterns. Applied changes needed to cleaning.py and main.py creation

---
+++ assignments/life_expectancy/main.py
@@ -0,0 +1,42 @@
+import argparse
+from pathlib import Path
+from pandas import DataFrame
+from life_expectancy.tests import OUTPUT_DIR
+from life_expectancy.region import Region
+from life_expectancy.cleaning import clean_data
+from life_expectancy.strategy import TSVCSVFilesStrategy, JSONFilesStrategy, save_data
+
+
+def main(input_path: str|Path,
+         output_path: str|Path,
+         region: Region = Region.PT
+        ) -> DataFrame:
+    
+    """
+    Function that have different approaches given the extension of the input file
+    """
+    if input_path.lower().endswith(('.zip')):
+        # if the extension is a .zip folder
+        df_loaded = JSONFilesStrategy().load_file(input_path)
+        df_cleaned = JSONFilesStrategy().clean_file(df_loaded, region)
+
+    else:
+        # if the extension is a .csv, .tsv, .txt
+        df_loaded = TSVCSVFilesStrategy().load_file(input_path)
+        df_cleaned = clean_data(df_loaded, region)
+    
+    return save_data(df_cleaned, output_path)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.description = "This is where the command-line utility's description goes."
+    parser.epilog = "This is where the command-line utility's epilog goes."
+    parser.add_argument('-i', default = f'{OUTPUT_DIR}/eu_life_expectancy_raw.tsv', help="You need to put here the path of the input file")
+    parser.add_argument('-r', type = Region, choices = Region, default = Region.PT, help = "Filter for the region you want to select.")
+    parser.add_argument('-o', default = f'{OUTPUT_DIR}/{parser.parse_args().r.value.lower()}_life_expectancy.csv', help="You need to put here the path where you want to write the output file")
+    args = parser.parse_args()
+    
+    main(input_path = args.i, region = args.r,  output_path = args.o)
+    
---
+++ assignments/life_expectancy/strategy.py
@@ -0,0 +1,82 @@
+from typing import Any, Protocol
+from pathlib import Path
+import pandas as pd
+from pandas import DataFrame
+from life_expectancy.region import Region
+
+
+class FileStrategy(Protocol):
+    """Reads Aand Cleans the file"""
+
+    def load_file(self, input_path:Any):
+        """
+        Reads the file and returns the data as a pandas DataFrame
+        Args:
+            input_path (Any): file object to read
+        """
+    def clean_file(self, df:Any, region:Any):
+        """
+        Cleans data and returns the data as a pandas DataFrame
+        Args:
+            df (Any): file object to clean
+            region (Any): region object to filter
+        """
+
+class TSVCSVFilesStrategy:
+    """Reads the TSV/CSV File"""
+
+    def load_file(self, input_path: str|Path) -> pd.DataFrame:
+        """
+        Reads a .CSV, .TXT file and returns the data as a pandas DataFrame
+        Args:
+            input_path (Any): path to the raw information
+        Returns:
+            pd.DataFrame: The data read from the .TSV, .CSV, .TXT file as a pandas DataFrame
+        """
+        return pd.read_csv(input_path, sep="[\t]", engine="python")
+
+
+class JSONFilesStrategy:
+    """Reads the TSV/CSV File"""
+
+    def load_file(self, input_path: str|Path) -> pd.DataFrame:
+        """
+        Reads a JSON input_path and returns the data as a pandas DataFrame
+        Args:
+            input_path: path to the raw information
+        Returns:
+            pd.DataFrame: The data read from the ZIP file as a pandas DataFrame
+        """
+
+        return pd.read_json(input_path, compression='zip', encoding='utf-8')
+    
+
+    def clean_file(self, df: DataFrame, region: Region = Region.PT) -> DataFrame:
+        """Function applied to load data if the file read is .json
+        :param df_loaded: DataFrame that was retrieved in load_file() function
+        :param region: define the region the user wants to filter. Default value = 'PT'.
+        :return df_cleaned: dataframe after some data cleaning
+
+        Notes: 
+            1. The .json file was no NaN or Null values in renamed value column.
+            2. Data don't need any much transformation besides column renaming and drop extra info.
+        """
+        df_renamed = df.rename(columns={"country": "region", "life_expectancy": "value"})
+
+        df_remove_columns = df_renamed.drop(columns=["flag", "flag_detail"])
+
+        df_cleaned = df_remove_columns[df_remove_columns['region'].str.lower() == region.value.lower()]
+
+        return df_cleaned    
+    
+
+
+    
+def save_data(df: pd.DataFrame, output_path: str|Path) -> None:
+    """Fuction that save the data into the expected folder as .csv
+    :param df: retrieved from data cleaning function applied. Cleaned information.
+    :param output_path: path where the file is saved. Search in init.py to find OUTPUT_DIR.
+    """
+    #Export that file into the folder
+    df.to_csv(output_path, index=False)
+    print(f"Finish data cleaning The file was saved as csv in:\n{output_path}\n")
